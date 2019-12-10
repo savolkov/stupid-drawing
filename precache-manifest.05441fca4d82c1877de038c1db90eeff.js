73f85db5fe2e0bd31892f0dec82293cdd653fea7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-10 14:08:03 +0000

    Deploy savolkov/stupid-drawing to github.com/savolkov/stupid-drawing.git:gh-pages

---
+++ precache-manifest.05441fca4d82c1877de038c1db90eeff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8aeaa1acfe2e88a56ae1e901081038",
+    "url": "/stupid-drawing/index.html"
+  },
+  {
+    "revision": "a85317027c1857447ac2",
+    "url": "/stupid-drawing/static/css/main.fb14547e.chunk.css"
+  },
+  {
+    "revision": "0e7d5b2ba771c6564fa6",
+    "url": "/stupid-drawing/static/js/2.6aa14faf.chunk.js"
+  },
+  {
+    "revision": "a85317027c1857447ac2",
+    "url": "/stupid-drawing/static/js/main.d3c54523.chunk.js"
+  },
+  {
+    "revision": "6b3f3246e7e2eee498a8",
+    "url": "/stupid-drawing/static/js/runtime-main.91e53bdd.js"
+  }
+]);
